f9681c73a852d984cd80cdd1a9b8c64e7f4fff39
ATISH KUMAR SAHU <95717588+AtishKumarSahu280301@users.noreply.github.com>
2022-11-10 18:33:54 +0530

    Add files via upload

---
+++ Assignment-4.c
@@ -0,0 +1,58 @@
+/*Answer-4_Assignment - 4 C Language LIVE Community Classes  MySirG*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	//1. Write a C program to print Hello Students on the screen.
+	printf("Ans1: \t");
+	printf("Hello Students");
+	printf("\n");
+	printf("\n");
+	
+	//2. Write a C Program to print Hello on the first line and Students in the second line.
+	printf("Ans2: \n");
+	printf("Hello \nStudents");
+	printf("\n\n");
+	
+	
+	//3. Write a C program to print “MySirG” on the screen
+	printf("Ans3: \t");
+	printf("\"MySirG\"");
+	printf("\n\n");
+	
+	//4. Write a C program to print “Teacher’s Day” on the screen
+	printf("Ans4: \t");
+	printf("\"Teacher\'s Day\"");
+	printf("\n\n");
+	
+	//5. Write a C program to print \n on the screen
+	printf("Ans5: \t");
+	printf("\\n");
+	printf("\n\n");
+	
+	//6. Write a C program to print %d on the screen
+	printf("Ans6: \t");
+	char ch = 37;
+	printf("%cd",ch);
+	printf("\n\n");
+	
+	/*7. Write a C program containing declaration of three variables (of type int, char and
+	float), also assign some values to them and print values of all three variables using
+	single printf().*/
+	printf("Ans7: \n");
+	int num1 = 185;
+	char ch1 = 'G';
+	float fl1 = 152.5;
+	printf("num1 = %d \tch1 = %c \tfl1 = %f",num1,ch1,fl1);	
+	printf("\n\n");
+	
+	/*9. Write a C program to print character stored in a char variable, also print its ASCII
+	code.*/
+	char ch2 = 'm';
+	printf("Ans9: \n");
+	printf("ch2= %c \t",ch2);
+	printf("ch2= %d",ch2);
+	printf("\n\n");
+	
+	getch();
+}
